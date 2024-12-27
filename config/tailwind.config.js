73d87af1a724561ed62814fb558d97b264b41fd0
ro-mo <1matteodeluca@gmail.com>
2024-12-27 19:09:34 +0100

    implemented notice and alert management

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -12,6 +12,15 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      animation: {
+        "appear-then-fade": "appear-then-fade 5s 300ms both",
+      },
+      keyframes: {
+        "appear-then-fade": {
+          "0%, 100%": { opacity: 0, transform: "translateY(100%)" },
+          "1%, 80%": { opacity: 1, transform: "translateY(0)" },
+        },
+      },
     },
   },
   plugins: [

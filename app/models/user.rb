ecbe9444c4754307e6b53a0f664877e59172d868
ro-mo <1matteodeluca@gmail.com>
2024-12-27 18:47:06 +0100

    add full name helper method

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   has_many :sessions, dependent: :destroy
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
+
+  def full_name
+    "#{first_name} #{last_name}".strip
+  end
 end

91d8ef51d8617c8dff16af5b497d47b54c1de1ba
ro-mo <1matteodeluca@gmail.com>
2025-02-06 09:39:24 +0100

    fixed patients signup

--- app/controllers/patients/registrations_controller.rb
+++ app/controllers/patients/registrations_controller.rb
@@ -1,6 +1,7 @@
 class Patients::RegistrationsController < RegistrationsController
   def new
     @patient = Patient.new
+    @doctors = Doctor.all.map { |d| [ "#{d.first_name} #{d.last_name}", d.id ] }
   end
 
   def create
@@ -9,13 +10,14 @@ class Patients::RegistrationsController < RegistrationsController
       start_new_session_for @patient
       redirect_to after_signup_path, notice: "Welcome! Your account has been created."
     else
+      @doctors = Doctor.all.map { |d| [ "#{d.first_name} #{d.last_name}", d.id ] }
       render :new, status: :unprocessable_entity
     end
   end
 
   private
     def patient_params
-      user_params
+      user_params.merge(doctor_id: params[:patient][:doctor_id])
     end
 
     def after_signup_path

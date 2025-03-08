bac8654e2a2ab30b05f7b0978a7e495686b40921
FuaPizza <mfoxer05@gmail.com>
2025-03-08 08:51:55 +0100

    Commit Login, Signup Frontend

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,16 +1,30 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   content: [
-    './public/*.html',
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    "./public/*.html",
+    "./app/helpers/**/*.rb",
+    "./app/javascript/**/*.js",
+    "./app/views/**/*.{erb,haml,html,slim}",
   ],
   theme: {
     extend: {
+      colors: {
+        marmorean: {
+          50: "#FFF8E1", // crema chiaro
+          100: "#FFECB3", // crema
+          200: "#FFE082", // oro pallido
+          300: "#FFD54F", // oro chiaro
+          400: "#FFCA28", // oro medio
+          500: "#FFC107", // oro standard
+          600: "#FFB300", // oro intenso
+          700: "#FFA000", // oro ambrato
+          800: "#FF8F00", // oro scuro
+          900: "#FF6F00", // oro brunito
+        },
+      },
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
       },
       animation: {
         "appear-then-fade": "appear-then-fade 5s 300ms both",
@@ -24,8 +38,8 @@ module.exports = {
     },
   },
   plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/container-queries'),
-  ]
-}
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/container-queries"),
+  ],
+};

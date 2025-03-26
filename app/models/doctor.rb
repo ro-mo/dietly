06b12367ee7444d3fa761d6d1b998a9dd81745a9
ro-mo <1matteodeluca@gmail.com>
2025-03-26 12:07:08 +0100

    Revert "added check albo biologi for doctors signup"
    This reverts commit 743e12a6b2c5e717a35e4c6a4d1dd2e5d0b62d0a.

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,16 +1,3 @@
 class Doctor < User
   has_many :patients, dependent: :nullify
-  validates :albo_id, presence: true
-
-  # La validazione dell'albo_id viene eseguita in background dopo la registrazione
-  # validate :albo_id_must_be_valid, on: :update
-
-  private
-
-  def albo_id_must_be_valid
-    verification_service = AlboVerificationService.new
-    unless verification_service.verify(albo_id)
-      errors.add(:albo_id, "non risulta registrato nell'Albo Unico Nazionale dei Biologi")
-    end
-  end
 end

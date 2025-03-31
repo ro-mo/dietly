b67679cbf62836b9bd2ebdda646d0a0541c7a8cd
Bergamo Giovanni <giovannibergamo259@gmail.com>
2025-03-31 19:13:47 +0200

    letter_opener

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,7 +31,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
-  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :letter_opener_web
   config.action_mailer.perform_deliveries = true
 
   # Log delle deprecazioni
--- config/routes.rb
+++ config/routes.rb
@@ -37,4 +37,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "home#show"
+
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
 end

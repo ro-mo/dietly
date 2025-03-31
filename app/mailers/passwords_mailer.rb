ed52d24849592788e388ad4b47b253ab7ae846c4
Bergamo Giovanni <giovannibergamo259@gmail.com>
2025-03-31 12:11:47 +0200

    deliver function

--- app/mailers/passwords_mailer.rb
+++ app/mailers/passwords_mailer.rb
@@ -3,18 +3,10 @@ class PasswordsMailer < ApplicationMailer
     @user = params[:user]
     @token = params[:token]
 
-    # Usa il metodo 'mail' per inviare l'email
-    if Rails.env.development?
-      mail(
-        to: @user.email_address,
-        subject: "Reset della tua password"
-      ).deliver_now
-    else
-      mail(
-        to: @user.email_address,
-        subject: "Reset della tua password"
-      ).deliver_later
-    end
+    mail(
+      to: @user.email_address,
+      subject: "Reset della tua password"
+    )
   end
 
   default from: "noreply@dietly.com"

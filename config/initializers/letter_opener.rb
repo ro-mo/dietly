d2e7ebed87ae5600eaaebe083f3455b4cacf0197
Bergamo Giovanni <giovannibergamo259@gmail.com>
2025-03-31 19:21:59 +0200

    letter_opener_rb

--- config/initializers/letter_opener.rb
+++
@@ -1,7 +0,0 @@
-# Configure letter_opener to use the default Windows browser
-if Rails.env.development?
-  Launchy.configure do |config|
-    config.browser = :default
-    config.application = "cmd.exe /c start"
-  end
-end 
